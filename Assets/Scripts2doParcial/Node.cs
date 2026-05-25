0eb0d3b8bc59373e5891dcb788d5c5d12b19ec42
Ivan Ramirez <ivan.r4amire5@gmail.com>
2026-05-25 01:45:15 -0300

    Plantilla basica de Node.cs

--- Assets/Scripts2doParcial/Node.cs
+++ Assets/Scripts2doParcial/Node.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
-
+using System.Collections.Generic;
 public class Node : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    
+    public List<Node> nodosConectados = new List<Node>();
+    public float radioDeBusqueda = 5f;
+    public LayerMask capaMuros;
+    [HideInInspector] public float distanceFromStart; //Representa el puntaje de distancia real recorrida 
+    [HideInInspector] public float estimatedDistanceToTarget; //Almacena la estimación en línea recta desde este nodo hasta el destino
+
+
+    public float finalScore
+    { 
+        get { return distanceFromStart + estimatedDistanceToTarget; } 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+    [HideInInspector] public Node nodoPadre;
 }
